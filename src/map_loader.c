1d1de03c2ac0070dc1e42405726cf101bbd9dea8
Tu Nombre <tu_email@example.com>
2025-07-01 12:50:26 +0200

    so_long 010725

--- src/map_loader.c
+++ src/map_loader.c
@@ -6,7 +6,7 @@
 /*   By: adruz-to <adruz-to@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 17:31:50 by adruz-to          #+#    #+#             */
-/*   Updated: 2025/06/27 17:32:30 by adruz-to         ###   ########.fr       */
+/*   Updated: 2025/07/01 12:49:17 by adruz-to         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 int	load_map(char *filename, t_map *map)
 {
-	
+	map->height = count_map_height(filename); // Calculamos la atura del mapa
+	if(map->height <= 0)
+	{
+		ft_printf("Error: Failed to calculate map height.\n");
+		return (0);
+	}
+	map->grid = allocate_map_grid(map->height); // asigna memroria para la matriz del mapa (grid)
+	if(!map->grid)
+		return (0);
+	// Llena la matriz del mapa con las líneas del archivo
+	if (!fill_map_grid(filename, map->grid, map->height))
+	{
+		ft_printf("Error: Failed to fill map grid.\n");
+		free(map->grid); // libera memoria si ocurre un error
+		return (0);
+	}
+	map->width = ft_strlen(map->grid[0]); // Calcula el ancho del mapa usando la primera línea
+	count_map_elements(map); // Cuenta los elementos importantes del mapa (C, P, E)
+	if (!validate_map_walls(map)) // Valida que el mapa esté rodeado de paredes
+	{
+		ft_printf("Error: Map is not surronded by walls.\n");
+		return (0);
+	}
+	return (1); // Devuelve 1 si el mapa cargó y validó correctamente
 }
+
--- src/map_utilities.c
+++ src/map_utilities.c
@@ -6,7 +6,7 @@
 /*   By: adruz-to <adruz-to@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 17:04:06 by adruz-to          #+#    #+#             */
-/*   Updated: 2025/06/27 17:33:11 by adruz-to         ###   ########.fr       */
+/*   Updated: 2025/07/01 12:38:07 by adruz-to         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,42 @@
 // Calcala la altura del mapa leyendo el archivo línea por línea
 int	count_map_height(char *filename)
 {
+	int		fd;
+	int		height;
+	char	*line;
 
+	height = 0;
+	fd = open(filename, O_RDONLY); // Abrir archivo en modo lectura
+	if (fd < 0)
+	{
+		ft_printf("Error: Could not open the file.\n");
+		return (-1); // Devuelve -1 en caso de error
+	}
+	while ((line = get_next_line(fd))) // Lee línea por línea y cuenta
+	{
+		free(line); // Liberamos memoria de la línea leída
+		height++;   // Incrementamos la altura por cada línea
+	}
+	clode(fd);       // Cerramos el archivo
+	return (height); // Devuelve la altura calculada
 }
 
 // Asigna memoria para la matriz map->grid
-char **allocate_map_grid(int height)
+char	**allocate_map_grid(int height)
 {
-
 }
 
 // Llena la matriz map->grid con las líneas del archivo
 int	fill_map_grid(char filename, char **grid, int height)
 {
-
 }
 
-//Cuenta los elementos importantes(C, P, E) en el mapa
+// Cuenta los elementos importantes(C, P, E) en el mapa
 void	count_map_elements(t_map *map)
 {
-
 }
 
 // Valida que el mapa esté rodeado de paredes
 int	validate_map_walls(t_map *map)
 {
-
 }
